348a47fddbb3471bf6e4aa08d56861655d17fe14
David Dobrovolný <dejv.dobrovolny@seznam.cz>
2024-11-04 13:00:02 +0100

    detailProvider is done

--- src/Providers/DetailProvider.js
+++ src/Providers/DetailProvider.js
@@ -4,13 +4,13 @@ export const DetailContext = createContext();
 
 function DetailProvider({ children }) {
   const [data, setData] = useState({
-    id: `${Math.random}`,
+    id: `${Math.random()}`,
     name: "Testing list",
     owner: "u1",
     memberList: ["u2", "u3"],
     itemList: [
       {
-        id: `${Math.random}`,
+        id: `${Math.random()}`,
         name: "Banana",
         resolved: false,
       },
@@ -41,28 +41,75 @@ function DetailProvider({ children }) {
           return { ...current };
         });
       },
-      updateItem: () => {
-        ({ id, name, quantity }) => {
-          setData((current) => {
-            const itemIndex = current.itemList.findIndex(
-              (item) => item.id === id
-            );
+      updateItem: ({ id, name, quantity }) => {
+        setData((current) => {
+          const itemIndex = current.itemList.findIndex(
+            (item) => item.id === id
+          );
 
-            current.itemList[itemIndex] = {
-              ...current.itemList[itemIndex],
-              name,
-              quantity,
-            };
-            return { ...current };
-          });
-        };
+          current.itemList[itemIndex] = {
+            ...current.itemList[itemIndex],
+            name,
+            quantity,
+          };
+          return { ...current };
+        });
       },
       updateListName: ({ name }) => {
+        setData((current) => ({
+          ...current,
+          name,
+        }));
+      },
+      resolveItem: ({ id }) => {
         setData((current) => {
-          current.name = name;
+          const itemIndex = current.itemList.findIndex(
+            (item) => item.id === id
+          );
+          current.itemList[itemIndex] = {
+            ...current.itemList[itemIndex],
+            resolved: !current.itemList[itemIndex].resolved,
+          };
+          return { ...current };
         });
-        return { ...current };
+      },
+      deleteItem: ({ id }) => {
+        setData((current) => {
+          const itemIndex = current.itemList.findIndex(
+            (item) => item.id === id
+          );
+          current.itemList.splice(itemIndex, 1);
+          return { ...current };
+        });
+      },
+      addMember: ({ memberId }) => {
+        setData((current) => {
+          if (!current.memberList.includes(memberId)) {
+            current.memberList.push(memberId);
+          }
+          return { ...current };
+        });
+      },
+
+      removeMember: ({ memberId }) => {
+        setData((current) => {
+          const memberIndex = current.memberList.findIndex(
+            (member) => member.id === memberId
+          );
+          if (memberIndex > -1) {
+            current.memberList.splice(memberIndex, 1);
+          }
+          return { ...current };
+        });
       },
     },
+    showResolved,
+    toggleShowResolved: () => setShowResolved((current) => !current),
   };
+
+  return (
+    <DetailContext.Provider value={value}>{children}</DetailContext.Provider>
+  );
 }
+
+export default DetailProvider;
