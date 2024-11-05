ad652ee58e7443efd67a59bbc387b7b9a9bc8cd3
David Dobrovolný <dejv.dobrovolny@seznam.cz>
2024-11-05 16:32:34 +0100

    resolve button is done

--- src/Detail/ResolveItemButton.js
+++ src/Detail/ResolveItemButton.js
@@ -1,19 +1,32 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import Button from "react-bootstrap/Button";
 import { DetailContext } from "../Providers/DetailProvider";
 
 function ResolveItemButton({ itemId }) {
   const { handlerMap } = useContext(DetailContext);
 
+  const [isBlocked, setIsBlocked] = useState(false);
+
+  const handleResolveItem = () => {
+    setIsBlocked(true);
+    handlerMap.resolveItem({ itemId });
+  };
+
   return (
-    <>
+    <div
+      style={{
+        opacity: isBlocked ? 0.5 : 1,
+        pointerEvents: isBlocked ? "none" : "auto",
+      }}
+    >
       <Button
         variant="primary"
-        onClick={() => handlerMap.resolveItem({ id: itemId })}
+        onClick={handleResolveItem}
+        disabled={isBlocked}
       >
         Resolve
-      </Button>{" "}
-    </>
+      </Button>
+    </div>
   );
 }
 
