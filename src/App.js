42687329f0a78be0e051a46bc72da0ee355eb647
David Dobrovolný <dejv.dobrovolny@seznam.cz>
2024-11-06 10:59:39 +0100

    detail button is workin with items functions

--- src/App.js
+++ src/App.js
@@ -3,15 +3,18 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Detail from "./Detail/Detail";
 import Overview from "./Overview/Overview";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import DetailProvider from "./Providers/DetailProvider";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Overview />} />
-        <Route path="/list/:listId" element={<Detail />} />
-      </Routes>
-    </Router>
+    <DetailProvider>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Overview />} />
+          <Route path="/list/:listId" element={<Detail />} />
+        </Routes>
+      </Router>
+    </DetailProvider>
   );
 }
 
